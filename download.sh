1c3b5237c8b8501db5ac8662e07d877f244bd16c
w0n9 <675920863@qq.com>
2021-04-27 22:25:11 +0800

    修改下载地址，供国内使用

--- download.sh
+++ download.sh
@@ -11,23 +11,23 @@ Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 FILE=$1
 
 if [ $FILE == "pretrained-network-celeba-hq" ]; then
-    URL=https://www.dropbox.com/s/96fmei6c93o8b8t/100000_nets_ema.ckpt?dl=0
+    URL=https://caddy.buct.edu.cn/stargan-v2/pretrained-network-celeba-hq_100000_nets_ema.ckpt
     mkdir -p ./expr/checkpoints/celeba_hq
     OUT_FILE=./expr/checkpoints/celeba_hq/100000_nets_ema.ckpt
     wget -N $URL -O $OUT_FILE
 
 elif  [ $FILE == "pretrained-network-afhq" ]; then
-    URL=https://www.dropbox.com/s/etwm810v25h42sn/100000_nets_ema.ckpt?dl=0
+    URL=https://caddy.buct.edu.cn/stargan-v2/pretrained-network-afhq_100000_nets_ema.ckpt
     mkdir -p ./expr/checkpoints/afhq
     OUT_FILE=./expr/checkpoints/afhq/100000_nets_ema.ckpt
     wget -N $URL -O $OUT_FILE
     
 elif  [ $FILE == "wing" ]; then
-    URL=https://www.dropbox.com/s/tjxpypwpt38926e/wing.ckpt?dl=0
+    URL=https://caddy.buct.edu.cn/stargan-v2/wing.ckpt
     mkdir -p ./expr/checkpoints/
     OUT_FILE=./expr/checkpoints/wing.ckpt
     wget -N $URL -O $OUT_FILE
-    URL=https://www.dropbox.com/s/91fth49gyb7xksk/celeba_lm_mean.npz?dl=0
+    URL=https://caddy.buct.edu.cn/stargan-v2/celeba_lm_mean.npz
     OUT_FILE=./expr/checkpoints/celeba_lm_mean.npz
     wget -N $URL -O $OUT_FILE
 
